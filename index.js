8aa1d3c60e171161b2b7f31ed57e0275d0497199
16_abhip <abhip16singh@gmail.com>
2022-05-03 11:44:22 +0530

    set static files including layout

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(expressLayouts);
 // use express router
+
+// extract style and script from sub pages into the layout
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 app.use('/', require('./routes'));
+// static files
+
+app.use(express.static('./assets'));
 
 // set up the view engine
 
