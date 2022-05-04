206ef8b0a7e0bb6c9031fe96738425f51e617b72
16_abhip <abhip16singh@gmail.com>
2022-05-04 22:19:44 +0530

    add sign up page and controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function(req,res)
 {
+    console.log(req.cookies);
+    res.cookie('user_id',25);
     res.render('home',{
         title: 'Home'
     });
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,9 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
 
-
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
+router.post('/create', usersController.create);
 // for any further routes, access from here
 // router.use('/routerName', require("./routerfile"))
 
