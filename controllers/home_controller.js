bf7a120345d5682f7637780bc370f72629c75d33
16_abhip <abhip16singh@gmail.com>
2022-05-08 02:40:49 +0530

    fetch post from db and populate user in posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,22 @@
+
+const Post = require('../models/post')
+
+
 module.exports.home = function(req,res)
 {
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    res.render('home',{
-        title: 'Home'
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,post){
+        if(err){
+            return console.log('Error in finding the post:', err);
+        }
+        res.render('home' ,{
+            title: 'Codeial | Home',
+            posts : post
+        })
     });
+
+    
 };
