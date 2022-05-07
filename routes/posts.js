61453751a3fa64f20c381b2da16a7137402813a1
16_abhip <abhip16singh@gmail.com>
2022-05-08 02:50:52 +0530

    restrict to user who is not sign in cannot post at two level 1. not showing form 2. at router check authentication

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { Passport } = require('passport/lib');
 const router = express.Router();
+const passport = require('passport');
 
 const postController = require('../controllers/posts_controller');
 
 console.log('post router is up');
 router.get('/like', postController.like);
-router.post('/create', postController.create);
+router.post('/create',passport.checkAuthentication, postController.create);
 module.exports = router;
